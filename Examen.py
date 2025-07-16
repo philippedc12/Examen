b577f9a587e9eae2b4aba6b3aa68b73681b1bd55
philippedc12 <ph.nowajewsky@duocuc.cl>
2025-07-15 23:17:52 -0400

    cambio linea 11

--- Examen.py
+++ Examen.py
@@ -7,7 +7,10 @@ productos = {'8475HD':['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia G
 '342FHD':['Acer', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
 'UWU131HD':['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'], 
 }
-cambio
+
+primer_cambio=1
+
+
 # productos = {modelo : [marca, pantalla, RAM, disco, GB de DD, procesador, video ...]}
 # (DD)= Mecanico y (SSD)= Estado solido
 stock = {'8475HD':[387990,10], 
